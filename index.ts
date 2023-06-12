e7fa15cd00aad6212decb81f39e9ca9911da2047
Willy <willymmerlet@gmail.com>
2023-06-12 16:49:04 -0400

    updated cors headers

--- index.ts
+++ index.ts
@@ -19,7 +19,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://futureblendai.com/');
+    res.header('Access-Control-Allow-Origin', "https://futureblend.herokuapp.com/");
+    res.header('Access-Control-Allow-Origin', 'https://futureblendai.com https://futureblend.herokuapp.com');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
