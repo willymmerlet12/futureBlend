6ac80748c3116e6c9b1455c27624756512d42f24
Willy <willymmerlet@gmail.com>
2023-06-13 10:29:57 -0400

    /

--- index.ts
+++ index.ts
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://futureblendai.com');
+  res.header('Access-Control-Allow-Origin', 'https://futureblendai.com/');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
