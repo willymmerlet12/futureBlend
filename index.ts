8a96b0366008f159bfe032e6fb4af018f8e4c40e
Willy <willymmerlet@gmail.com>
2023-06-21 12:59:31 -0400

    cors config

--- index.ts
+++ index.ts
@@ -51,8 +51,8 @@ const io = new SocketIO(httpsServer, {
 
 app.use(cors({
   origin: 'https://futureblendai.com',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Authorization'],
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
   credentials: true
 }));
 
@@ -62,13 +62,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.use(authMiddleware.decodeToken);
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
 
 //ERROR HANDLING
 app.use(function(err, req, res, next) {
